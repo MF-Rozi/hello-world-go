62e6e181448e2b9c4695fbf0d81a073eb0d635aa
MF-Rozi <m.fakhrulrozi10@gmail.com>
2025-07-25 22:23:34 +0700

    add: RandomGreetings..

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,11 +3,23 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Greet(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("Name cannot be empty")
+		return "", errors.New("name cannot be empty")
 	}
 	return fmt.Sprintf("Hello, %s!", name), nil
+}
+
+func RandomGreet() string {
+	format := []string{
+		"Hello, Sir!",
+		"Greetings, Friend!",
+		"Salutations, Traveler!",
+		"Welcome, Guest!",
+		"Hi there, Buddy!",
+	}
+	return format[rand.Intn(len(format))]
 }
--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -17,4 +17,6 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
+
+	fmt.Println(greetings.RandomGreet())
 }
