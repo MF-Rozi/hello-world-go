ea42803a9b53631f208f7858a893a9d836e94e69
MF-Rozi <m.fakhrulrozi10@gmail.com>
2025-07-25 22:18:17 +0700

    add: implement error catching

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,13 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
-func Greet(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+func Greet(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Name cannot be empty")
+	}
+	return fmt.Sprintf("Hello, %s!", name), nil
 }
--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"dev.mfr/greetings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	fmt.Println(greetings.Greet("Genjirou"))
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Greet("Genjirou")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
