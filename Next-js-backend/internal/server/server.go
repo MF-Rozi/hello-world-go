000d39cd9a078bf16a0c21da4c15b6f9dbdc3025
MF-Rozi <m.fakhrulrozi10@gmail.com>
2025-09-11 13:21:14 +0700

    add: server and routes

--- Next-js-backend/internal/server/server.go
+++ Next-js-backend/internal/server/server.go
@@ -1,5 +1,22 @@
 package server
 
-// This file is intentionally left empty for now.
-
 //TODO: - make Chi Server and Routes
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func routes() *chi.Mux {
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to the Next.js Backend!")
+	})
+	return r
+}
+func StartServer() {
+	r := routes()
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+}
