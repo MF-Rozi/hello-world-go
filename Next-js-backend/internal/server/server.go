13e73e09e683f0759e0766316deb4335ebe58ff2
MF-Rozi <m.fakhrulrozi10@gmail.com>
2025-09-12 21:14:59 +0700

    add: running  server in main.go

--- Next-js-backend/internal/server/server.go
+++ Next-js-backend/internal/server/server.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes() *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Welcome to the Next.js Backend!")
 	})
@@ -17,6 +19,7 @@ func routes() *chi.Mux {
 }
 func StartServer() {
 	r := routes()
+
 	fmt.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", r)
 }
