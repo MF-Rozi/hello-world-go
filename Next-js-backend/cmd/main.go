1ad3a9aa885b1d9988744a5d39c2831ea2a28468
MF-Rozi <m.fakhrulrozi10@gmail.com>
2025-09-15 23:11:52 +0700

    add: load .env before start the server

--- Next-js-backend/cmd/main.go
+++ Next-js-backend/cmd/main.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 
 	"dev.mfr/next-js-backend/internal/server"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load environment variables from .env file
+	if err := godotenv.Load("internal/config/.env"); err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
 	fmt.Println("Hello, World!")
 
 	server.StartServer()
--- Next-js-backend/go.mod
+++ Next-js-backend/go.mod
@@ -2,4 +2,7 @@ module dev.mfr/next-js-backend
 
 go 1.24.5
 
-require github.com/go-chi/chi/v5 v5.2.3
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/joho/godotenv v1.5.1
+)
