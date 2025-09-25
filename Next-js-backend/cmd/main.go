41329549ebb16e62c72fc4a47a6207b5bafa7bf4
MF-Rozi <m.fakhrulrozi10@gmail.com>
2025-09-25 14:17:46 +0700

    add: instruction to run the server

--- Next-js-backend/cmd/main.go
+++ Next-js-backend/cmd/main.go
@@ -9,8 +9,12 @@ import (
 )
 
 func main() {
+	// to start the server go to root directory and run `go run cmd/main.go`
+	fmt.Println("Starting the application...")
+	env_url := "internal/config/.env"
+	fmt.Println("Loading environment variables from: ", env_url)
 	// load environment variables from .env file
-	if err := godotenv.Load("internal/config/.env"); err != nil {
+	if err := godotenv.Load(env_url); err != nil {
 		fmt.Println("Error loading .env file")
 		return
 	}
